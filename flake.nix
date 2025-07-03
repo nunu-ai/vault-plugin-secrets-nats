a0cceb783ff90db0bae3688e12745c9da9bff901
jannik <jannik@nunu.ai>
2025-07-03 08:58:45 +0000

    add pre-commit

--- flake.nix
+++ flake.nix
@@ -3,17 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, pre-commit-hooks, ... }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     in {
       devShells = forAllSystems (system:
-        let 
+        let
           pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true;
+          };
+
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              gofmt.enable = true;
+              goimports.enable = true;
+            };
           };
         in {
           default = pkgs.mkShell {
@@ -23,9 +32,11 @@
               docker
               gnumake
               just
+              pre-commit
             ];
-            
+
             shellHook = ''
+              ${pre-commit-check.shellHook}
               export VAULT_ADDR='http://127.0.0.1:8200'
               export VAULT_TOKEN='root'
             '';
