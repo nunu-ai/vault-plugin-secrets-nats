7e1fc75ef084f200a695e7d2b1332c124adefd75
Thomas <thomas@nunu.ai>
2026-07-13 12:14:10 +0200

    fix: do not do custom tls

--- cmd/vault-plugin-secrets-nats/main.go
+++ cmd/vault-plugin-secrets-nats/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"os"
-
 	nats "github.com/edgefarm/vault-plugin-secrets-nats"
-	"github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/sdk/plugin"
 
 	"github.com/rs/zerolog"
@@ -14,19 +11,10 @@ import (
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	apiClientMeta := &api.PluginAPIClientMeta{}
-	flags := apiClientMeta.FlagSet()
-	flags.Parse(os.Args[1:])
-
-	tlsConfig := apiClientMeta.GetTLSConfig()
-	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
-
 	err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: nats.Factory,
-		TLSProviderFunc:    tlsProviderFunc,
 	})
 	if err != nil {
 		log.Error().Err(err).Msg("plugin shutting down")
-		os.Exit(1)
 	}
 }
